a8dfdac431ff5e8ddb9e7da5555da31e5db3c7ae
anette1983 <105170538+anette1983@users.noreply.github.com>
2022-10-09 17:40:00 +0300

    images optimized

---
+++ js/slider.js
@@ -0,0 +1,18 @@
+// $('.fade').slick({
+//   dots: true,
+//   autoplay: true,
+//   autoplaySpeed: 3000,
+//   infinite: true,
+//   speed: 500,
+//   fade: true,
+//   cssEase: 'linear'
+// });
+$('.multiple-items').slick({
+  dots: true,
+  dotsClass: "my-dots",
+  // autoplay: true,
+  autoplaySpeed: 3500,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
+});
